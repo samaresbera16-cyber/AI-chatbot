cd126b800b6aa73a53c1872eaba1a899cc87803e
samaresbera16-cyber <samaresbera16@gmail.com>
2026-05-09 12:41:55 +0530

    fixed render deploy

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-
+import os
 app = Flask(__name__)
 
 # Simple chatbot responses
@@ -314,5 +314,5 @@ def chat():
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
